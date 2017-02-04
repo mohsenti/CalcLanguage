6ef03e1dfc95a967ae7d3f6af19de96771d55b9e
mohsen <mohsen.timar@gmail.com>
2017-02-04 18:51:46 +0330

    Add dot graph generator

--- main.c
+++ main.c
@@ -2,11 +2,27 @@
 #include "scanner.h"
 #include "parser.tab.h"
 
+int dotIndex = 1;
+
+void buildNodeDotTree(FILE *file, int parentIndex, ParserTreeNode *node) {
+    int thisID = dotIndex++;
+    fprintf(file, "\t%d -> %d \n", parentIndex, thisID);
+    for (ParserTreeNode *i = node->firstChild; i != NULL; i = i->next) {
+        buildNodeDotTree(file, thisID, i);
+    }
+}
+
+void buildDotTree(char *fileName) {
+    FILE *file = fopen(fileName, "w");
+    fprintf(file, "digraph graphname {\n");
+    buildNodeDotTree(file, 0, root);
+    fprintf(file, "}");
+}
+
 int main() {
     yyin = fopen("example.calc", "r");
-    //do {
     yyparse();
-    //} while (!feof(yyin));
     fclose(yyin);
+    buildDotTree("parseTree.dot");
     return 0;
 }
