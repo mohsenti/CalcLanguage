7888ea165ec1984aa89667658ade3fe7062213d7
mohsen <mohsen.timar@gmail.com>
2017-02-04 16:04:26 +0330

    add parser tree structures

--- parser_utils.h
+++ parser_utils.h
@@ -5,6 +5,26 @@
 #ifndef CALCLANGUAGE_PARSER_UTILS_H
 #define CALCLANGUAGE_PARSER_UTILS_H
 
+
+typedef union ParserTreeNodeData ParserTreeNodeData;
+union ParserTreeNodeData {
+    double dValue;
+    char operator;
+    char *Id;
+    char *functionName;
+};
+
+
+typedef struct ParserTreeNode ParserTreeNode;
+struct ParserTreeNode {
+    double value;
+    int type;
+    ParserTreeNodeData data;
+    ParserTreeNode *firstChild;
+    ParserTreeNode *next;
+};
+
+
 int yywrap(void);
 
 int yyerror(const char *error);
